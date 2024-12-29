96a470ec4897d3e1e608eff7238346352fb55e16
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-29 17:14:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,8 +38,11 @@ btn.addEventListener("click",()=>{
 			   let b = ""
 			   let c = ""
 			   let d = ""
-			   let e = ""
+			   let x = ""
 			   let f = ""
+			   let g = ""
+			   let h = ""
+			   let k = ""
 			   for(let i=0; i<=box.length-1; i++){
 				   if(i==0 || i==1 || i==2){
 					   b+=box[i].textContent;
@@ -47,19 +50,28 @@ btn.addEventListener("click",()=>{
 				   if(i==0|| i==3||i==6){ 
 					   c+=box[i].textContent;
 				   }
-				   if(i==0||i==5||i==9){
+				   if(i==0||i==4||i==8){
 					   d+=box[i].textContent;
 				   }
 				   if(i==1||i==4||i==7){
-					   e+=box[i].textContent;
+					   x+=box[i].textContent;
 				   }
 				   if(i==2||i==5||i==8){
 					   f+=box[i].textContent;
 				   }
-				   
+				   if(i==3||i==4||i==5){
+					   g+=box[i].textContent; 
+				   }
+				   if(i==6||i==7||i==8){
+					   h+=box[i].textContent;
+				   }
+				   if(i==2||i==4||i==6){
+					   k+=box[i].textContent;
+				   }
 			   } 
-			   if(b=="xxx" || c=="xxx" ||d=="xxx" ||e=="xxx"||f=="xxx"){ 
+			   if(b=="xxx" || c=="xxx" ||d=="xxx" ||x=="xxx"||f=="xxx"||g=="xxx"||h=="xxx"||k=="xxx"){ 
 					   message.textContent = `${a} is Winner`
+				    grid1.removeEventListener("click",call)
 				   }
 			   j++
 		   }else{
@@ -70,7 +82,44 @@ btn.addEventListener("click",()=>{
 		   if(e.target.closest(".box").textContent.length==0){
 			   e.target.closest(".box").textContent = "0"
 			   message.textContent = `${b}, you're up`
-			   
+			    let b = ""
+			   let c = ""
+			   let d = ""
+			   let x = ""
+			   let f = ""
+			   let g = ""
+			   let h = ""
+			   let k = ""
+			   for(let i=0; i<=box.length-1; i++){
+				   if(i==0 || i==1 || i==2){
+					   b+=box[i].textContent;
+				   }
+				   if(i==0|| i==3||i==6){ 
+					   c+=box[i].textContent;
+				   }
+				   if(i==0||i==4||i==8){
+					   d+=box[i].textContent;
+				   }
+				   if(i==1||i==4||i==7){
+					   x+=box[i].textContent;
+				   }
+				   if(i==2||i==5||i==8){
+					   f+=box[i].textContent;
+				   }
+				   if(i==3||i==4||i==5){
+					   g+=box[i].textContent; 
+				   }
+				   if(i==6||i==7||i==8){
+					   h+=box[i].textContent;
+				   }
+				   if(i==2||i==4||i==6){
+					   k+=box[i].textContent;
+				   }
+			   } 
+			   if(b=="000" || c=="000" ||d=="000" ||x=="000"||f=="000"||g=="000"||h=="000"||k=="000"){ 
+					   message.textContent = `${b} is Winner`
+				    grid1.removeEventListener("click",call)
+				   }
 			   j++
 		   }else{
 			   j;
